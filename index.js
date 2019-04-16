434d1aec4f1d5e6d3fb55cd14a61719c9bd25a48
Alex Suarez <alexsuarezmail1@gmail.com>
2019-04-16 18:15:23 -0500

    added helmet

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 require("./startup/logging")();
 require("./startup/cors")(app);
+require("./startup/helmet")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
